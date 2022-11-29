6d1bb378d998713055296536ecafedb1e1d29dda
brady.wiseman <brady.wiseman@mymhc.ca>
2022-11-29 12:11:46 -0700

    Add project files.

---
+++ DesertBusMode7/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DesertBusMode7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pBoxRender = new System.Windows.Forms.PictureBox();
+            this.timerRender = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRender)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBoxRender
+            // 
+            this.pBoxRender.BackColor = System.Drawing.Color.Black;
+            this.pBoxRender.Location = new System.Drawing.Point(-3, 171);
+            this.pBoxRender.Name = "pBoxRender";
+            this.pBoxRender.Size = new System.Drawing.Size(1258, 488);
+            this.pBoxRender.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxRender.TabIndex = 0;
+            this.pBoxRender.TabStop = false;
+            // 
+            // timerRender
+            // 
+            this.timerRender.Enabled = true;
+            this.timerRender.Interval = 10;
+            this.timerRender.Tick += new System.EventHandler(this.timerRender_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(1252, 565);
+            this.Controls.Add(this.pBoxRender);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRender)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pBoxRender;
+        private System.Windows.Forms.Timer timerRender;
+    }
+}
---
+++ DesertBusMode7/Form1.cs
@@ -0,0 +1,252 @@
+namespace DesertBusMode7
+{
+    public partial class Form1 : Form
+    {
+        //These variables are all used to render                                                 
+        //the 3D plane. The worldx and worldy variables
+        //are modified by moving forward or backward for y,
+        //and left or right for x. During the normal game,
+        //all others are not controlled by the player,
+        //but may change based on what is happening in
+        //the game. In the Map View mode, the player has
+        //more or less free control over non calculated
+        //variables.
+        Bitmap PlaneMap1 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap2 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap3 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap4 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap5 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap6 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap7 = new Bitmap(Properties.Resources.DesertMain);
+        Bitmap PlaneMap8 = new Bitmap(Properties.Resources.DesertMain);  //This is a super scuffed way
+        Bitmap Render = new Bitmap(250, 250);                            //of doing this but I can't
+                                                                         //spend all my time working
+        Bitmap DesertMain = new Bitmap(Properties.Resources.DesertMain); //on the graphics
+        Bitmap Desert1 = new Bitmap(Properties.Resources.Desert1);
+        Bitmap Desert2 = new Bitmap(Properties.Resources.Desert2);
+        Bitmap KoopaBeachMain = new Bitmap(Properties.Resources.KoopaBeachMain);
+        Bitmap ChocoMtMain = new Bitmap(Properties.Resources.ChocoMtMain);
+        Bitmap VanillaLakeMain = new Bitmap(Properties.Resources.VanillaLakeMain);
+        Bitmap GhostValleyMain = new Bitmap(Properties.Resources.GhostValleyMain);
+        Bitmap BowserCastleMain = new Bitmap(Properties.Resources.BowserCastleMain);
+        Bitmap RainbowRoadMain = new Bitmap(Properties.Resources.RainbowRoadMain);
+        double worldx = 0.5;
+        double worldy = 0.01;
+        double worlda = 1.55;
+        double fnear = 0.01;
+        double ffar = 0.4;
+        double fovhalf = 3.1415 / 4;
+        double ffarx1;
+        double ffary1;
+        double ffarx2;
+        double ffary2;
+        double fnearx1;
+        double fneary1;
+        double fnearx2;
+        double fneary2;
+
+        //The following variables are more game related, including the speed of the bus,
+        //how long you've been driving, ect.
+        bool GasPedal;
+        double BusSpeed;
+        double BusDrift;
+        int TimeDriven; //in Minutes
+
+
+
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();   
+            Mode7Render();
+        }
+
+       
+        private void Mode7Render()
+        {
+            ffarx1 = (worldx + (Math.Cos(worlda - fovhalf)) * ffar);
+            ffary1 = (worldy + (Math.Sin(worlda - fovhalf)) * ffar);
+
+            ffarx2 = (worldx + (Math.Cos(worlda + fovhalf)) * ffar);
+            ffary2 = (worldy + (Math.Sin(worlda + fovhalf)) * ffar);
+
+            fnearx1 = worldx + (Math.Cos(worlda - fovhalf)) * fnear;
+            fneary1 = worldy + (Math.Sin(worlda - fovhalf)) * fnear;
+
+            fnearx2 = worldx + (Math.Cos(worlda + fovhalf)) * fnear;
+            fneary2 = worldy + (Math.Sin(worlda + fovhalf)) * fnear;
+
+            
+
+            for (int y = 0; y < Render.Height-120; y+=2)
+            {
+                double depth = ((double)y / ((double)Render.Height));
+
+                double fstartX = ((ffarx1 - fnearx1) / (depth)) + fnearx1;
+                double fstartY = ((ffary1 - fneary1) / (depth)) + fneary1;
+                double fendX = ((ffarx2 - fnearx2) / (depth)) + fnearx2;
+                double fendY = ((ffary2 - fneary2) / (depth)) + fneary2;
+                for (int x = 0; x < Render.Width; x+=1)
+                {
+                    double width = ((double)x / ((double)Render.Width));
+
+                    double samplex = (fendX - fstartX) * width + fstartX;
+                    double sampley = (fendY - fstartY) * width + fstartY;
+
+                    int pixel1 = (int)((samplex * PlaneMap1.Width));
+                    int pixel2 = (int)((sampley * PlaneMap1.Height));
+
+                    if (pixel1 > 151 || pixel1 < 1)
+                    {
+                        pixel1 = pixel1 - (9 * ((int)Math.Floor((double)pixel1 / 9)));
+                    }
+
+
+                    switch((int)Math.Floor((double)pixel2/PlaneMap1.Height))
+                    {
+                        case 0:
+                            Render.SetPixel(x, y, PlaneMap1.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap1.GetPixel(pixel1, pixel2));
+                            break;
+                        case 1:
+                            pixel2 = pixel2 - (PlaneMap1.Height);
+                            Render.SetPixel(x, y, PlaneMap2.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap2.GetPixel(pixel1, pixel2));
+                            break;
+                        case 2:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 2);
+                            Render.SetPixel(x, y, PlaneMap3.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap3.GetPixel(pixel1, pixel2));
+                            break;
+                        case 3:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 3);
+                            Render.SetPixel(x, y, PlaneMap4.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap4.GetPixel(pixel1, pixel2));
+                            break;
+                        case 4:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 4);
+                            Render.SetPixel(x, y, PlaneMap5.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap5.GetPixel(pixel1, pixel2));
+                            break;
+                        case 5:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 5);
+                            Render.SetPixel(x, y, PlaneMap6.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap6.GetPixel(pixel1, pixel2));
+                            break;
+                        case 6:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 6);
+                            Render.SetPixel(x, y, PlaneMap7.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap7.GetPixel(pixel1, pixel2));
+                            break;
+                        case 7:
+                            pixel2 = pixel2 - (PlaneMap1.Height * 7);
+                            Render.SetPixel(x, y, PlaneMap8.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap8.GetPixel(pixel1, pixel2));
+                            break;
+                        default:
+                            pixel2 = pixel2 - (144 * ((int)Math.Floor((double)pixel2 / 144)));
+                            Render.SetPixel(x, y, PlaneMap8.GetPixel(pixel1, pixel2));
+                            Render.SetPixel(x, y+1, PlaneMap8.GetPixel(pixel1, pixel2));
+                            break;
+
+
+
+
+                    }
+
+                  
+                }
+            }
+            pBoxRender.Image = Render;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                   GasPedal = true;
+                    break;
+                case Keys.Left:
+                    BusDrift = -0.005;
+                    break;
+                case Keys.Right:
+                    BusDrift = 0.005;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    GasPedal = false;
+                    break;
+                case Keys.Left:
+                    BusDrift = 0.0008;
+                    break;
+                case Keys.Right:
+                    BusDrift = 0.0008;
+                    break;
+            }
+        }
+
+        private void timerRender_Tick(object sender, EventArgs e)
+        {
+            if (GasPedal== true && BusSpeed < 0.05)
+            {
+                BusSpeed += 0.002;
+            }
+            if(GasPedal == false && BusSpeed > 0.00)
+            {
+                BusSpeed -= 0.0005;
+            }
+
+
+            worldy += BusSpeed;
+            worldx -= BusDrift;
+            
+            if (worldy < 0.00)
+            {
+                worldy = 0.99;
+            }
+            else if (worldy > 1.00)
+            {
+                worldy = 0.01;
+                PlaneMap1 = PlaneMap2;
+                PlaneMap2 = PlaneMap3;
+                PlaneMap3 = PlaneMap4;
+                PlaneMap4 = PlaneMap5;
+                PlaneMap5 = PlaneMap6;
+                PlaneMap6 = PlaneMap7;
+                PlaneMap7 = PlaneMap8;
+
+                switch (rnd.Next(1, 2)){
+                    case 1:
+                        PlaneMap8 = RainbowRoadMain;
+                        break;
+                    case 2:
+                        PlaneMap8 = Desert2;
+                        break;
+                    default:
+                        PlaneMap8 = DesertMain;
+                        break;
+                }
+            }
+            if (worldx > 1.00)
+            {
+                worldx = 0.01;
+            }
+            else if (worldx < 0.00)
+            {
+                worldx = 0.99;
+            }
+            Mode7Render();
+        }
+     
+    }
+}
